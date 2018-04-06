605a1767098afc1934cfbf2944195e1199e8c503
Maryam <mkarimif@uwo.ca>
2018-04-06 13:28:08 -0400

    operation classes updated

--- cs2212/src/Administrator.java
+++ cs2212/src/Administrator.java
@@ -1,41 +1,22 @@
 
 public class Administrator implements SystemUser {
 	
-	private String password;
 	private String adminID;
-	private String username;
 	private String name;
 	private String surname;
-	private String email;
 	private String birthday;
 	
-	public Administrator(String name,String surname, String ID,String password, String email, String username) {
-	
-		this.name = name;
-		this.surname = surname;
+	public Administrator(String ID) {
 		adminID = ID;
-		this.email = email;
-		this.username = username;
-		this.password = password;
 	}
-	public String getPassword(){
-		return this.password;
-	}
-	public void setPassword(String password){
-		this.password = password;
-	}
+	
 	public String getID(){
 		return this.adminID;
 	}
 	public void setID(String ID){
 		this.adminID = ID;
 	}
-	public String getAdministratorUserName(){
-		return this.username;
-	}
-	public void setAdministratorUserName(String username){
-		this.username = username;
-	}
+
 	public String getName(){
 		return this.name;
 	}
@@ -47,13 +28,6 @@ public class Administrator implements SystemUser {
 	}
 	public void setSurname(String surname){
 		this.surname = surname;
-	}
-	
-	public String getEmail(){
-		return this.email;
-	}
-	public void setEmail(String email){
-		this.email = email;
 	}
 	
 	public String getBirthday() {
